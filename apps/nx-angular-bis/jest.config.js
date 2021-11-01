40e50a77365b85e232ba9333d3fe153635834c51
François Bigot <fr4ngus@hotmail.fr>
2021-11-01 21:28:05 +0100

    add new app

---
+++ apps/nx-angular-bis/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  displayName: 'nx-angular-bis',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/nx-angular-bis',
+  coverageReporters: ["clover", "json", "lcov", ["text", {"skipFull": true}]],
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
